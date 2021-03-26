dddbef3239ecf459511f1b52cd36bd2cb979eb51
assimatagba <assima.tagba@epitech.eu>
2021-03-26 13:26:15 +0100

    CarItem component

---
+++ components/CarItem/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    // container: {
+    //     flex: 1,
+    //     backgroundColor: '#fff',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //   },
+    carContainer: {
+        width: '100%',
+        height: '100%',
+        resizeMode:'cover',
+        position: 'absolute'
+      },
+      
+      titles: {
+        marginTop: '30%',
+        width: '100%',
+        alignItems: 'center'
+      }, 
+    
+      title: {
+        fontSize: 40,
+        fontWeight: 'bold'
+      },
+    
+      subtitle: {
+        fontSize: 12,
+        color: 'grey'
+      },
+    
+      image: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute'
+      }
+})
+
+export default styles;
