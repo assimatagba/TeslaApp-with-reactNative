b890bb4e402f6154c28892b546eae3cac6415ca9
assimatagba <assima.tagba@epitech.eu>
2021-03-26 15:25:52 +0100

    Header component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,45 +1,50 @@
-import React from 'react'
-import { Text, View, ImageBackground } from 'react-native'
-import Styledbutton from '../shared/StyledButton'
-import styles from './styles'
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from "../StyledButton";
+import styles from './styles';
 
 const CarItem = (props) => {
-    const { name, tagLine, tagLineCTA, image } = props;
 
-    return (
-        
-        <View style={styles.carContainer}>
-            <ImageBackground 
-                source={image} 
-                style={styles.image}
-            />
+  const { name, tagline, taglineCTA, image } = props.car;
 
-            <View style={styles.titles}>
-                <Text style={styles.title}>{name}</Text>
-                <Text style={styles.subtitle}>
-                    {tagLine} 
-                    <Text style={styles.subtitleCTA}>
-                        {tagLineCTA}
-                    </Text>
-                </Text>
-            </View>
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={image}
+        style={styles.image}
+      />
 
-            <View style={styles.buttomContainer}>
-                <Styledbutton type="primary" 
-                    content={"Custom Order"} 
-                    onPress={() => {
-                        console.warn("Custom order was pressed")
-                    }}
-                />
-                <Styledbutton type="secondary"
-                    content={"Existing inventory"} 
-                    onPress={() => {
-                        console.warn("Existing inventory was pressed")
-                    }}
-                />
-            </View>
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline}
+          {' '}
+          <Text style={styles.subtitleCTA}>
+            {taglineCTA}
+          </Text>
+        </Text>
+      </View>
 
-        </View>
-    )
-}
-export default CarItem
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Custom Order"}
+          onPress={() => {
+            console.warn("Custom Order was pressed");
+          }}
+        />
+
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory was pressed");
+          }}
+        />
+      </View>
+
+    </View>
+  );
+};
+
+export default CarItem;
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,48 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
-    // container: {
-    //     flex: 1,
-    //     backgroundColor: '#fff',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    //   },
-    carContainer: {
-        width: '100%',
-        height: '100%',
-        resizeMode:'cover',
-        position: 'absolute'
-      },
-      
-      titles: {
-        marginTop: '30%',
-        width: '100%',
-        alignItems: 'center'
-      }, 
-    
-      title: {
-        fontSize: 40,
-        fontWeight: 'bold'
-      },
-    
-      subtitle: {
-        fontSize: 12,
-        color: 'grey'
-      },
-    
-      image: {
-        width: '100%',
-        height: '100%',
-        position: 'absolute'
-      },
-      buttomContainer: {
-        position: 'absolute',
-        bottom: 50,
-        width: '100%'
-      },
-      subtitleCTA: {
-        textDecorationLine: 'underline'
-      }
-})
+  carContainer: {
+    width: '100%',
+    height: Dimensions.get('window').height,
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitleCTA: {
+    textDecorationLine: 'underline',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  },
+
+  buttonsContainer: {
+    position: 'absolute',
+    bottom: 50,
+    width: '100%',
+  }
+});
 
 export default styles;
